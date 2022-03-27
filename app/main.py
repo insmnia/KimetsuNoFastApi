625e4bb1b49588506ee941e94b53d07f4653ea5e
insmnia <teamspirit3209@gmail.com>
2022-03-27 11:10:15 +0300

    Unautorized error handler

--- app/main.py
+++ app/main.py
@@ -1,24 +1,19 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import PlainTextResponse
+from fastapi import FastAPI
 
 from app.api.v1.api import router as api_router
 from app.core.config import get_settings
 from app.db.mongodb_utils import connect, close_connection
+from app.core.middleware import MaintainceModeMiddleware
+from app.core.errors import unauthorized_exception_handler
 
 app = FastAPI(title="KimetsuNoFastApi", debug=True)
 settings = get_settings()
 
 
-@app.middleware('http')
-async def maintaince_mod(request: Request, call_next):
-    response = await call_next(request)
-    if str(request.url).endswith('admin/'):
-        return response
-    if settings.MAINTAINCE_MODE:
-        return PlainTextResponse(content='Sorry... We now on a maintaince')
-    return response
-
-
 app.add_event_handler('startup', connect)
 app.add_event_handler('shutdown', close_connection)
+
+app.add_middleware(MaintainceModeMiddleware)
+app.exception_handler(unauthorized_exception_handler)
+
 app.include_router(api_router, prefix="/api")
