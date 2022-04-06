96efc7ccb6366b670d3ade1f2f1782dcf37d7ac5
insmnia <64920575+insmnia@users.noreply.github.com>
2022-04-06 15:25:34 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,13 +1,2 @@
-from app.core.services.user import UserService
-from conftests import UserFactory
-
-
-def test_password_hashing():
-    user = UserFactory.stub()
-    hashed_password = UserService.get_password_hash(user.password)
-
-    assert hashed_password is not None
-
-    
 def test_password_hash_matching():
     assert 1 == 1
